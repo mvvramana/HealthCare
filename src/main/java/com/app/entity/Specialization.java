781f4b6e012363aac6d54e401612c86bfb57107d
mvvramana <srinumedisetti35@gmail.com>
2021-10-20 19:03:35 +0530

    spec-repo and test case

--- src/main/java/com/app/entity/Specialization.java
+++ src/main/java/com/app/entity/Specialization.java
@@ -7,8 +7,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name="spec_tab")
 public class Specialization {
---
+++ src/test/java/com/app/repo/test/SpecializationRepositoryTest.java
@@ -0,0 +1,57 @@
+package com.app.repo.test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import com.app.entity.Specialization;
+import com.app.repo.SpecializationRepository;
+
+@DataJpaTest(showSql=true)
+@AutoConfigureTestDatabase(replace=Replace.NONE)
+@Rollback(false)
+@TestMethodOrder(OrderAnnotation.class)
+public class SpecializationRepositoryTest {
+	@Autowired
+	private SpecializationRepository repo;
+	
+	/*
+	 * 1. Test save Operation
+	 * */
+
+	@Test
+	@Order(1)
+	public void testSpecCreate() {
+		Specialization spec=new Specialization(null, "CRDLS", "cardiologists", "They're experts on the heart and blood vessels");
+		Specialization specOutput=repo.save(spec);
+		assertNotNull(specOutput.getId(),"spec is not created");
+	}
+	/*
+	 * 2. Test display all operation
+	 * */
+	@Test
+	@Order(2)
+	public void testFetchAll() {
+		List<Specialization> list=repo.findAll();
+		assertNotNull(list);
+		if(list.isEmpty()) {
+			fail("No data exist in database");
+		}
+			
+		
+	}
+	
+
+}
