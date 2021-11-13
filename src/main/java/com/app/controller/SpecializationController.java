885db7d8a06ef5c989dd8d50de3735199fad4603
mvvramana <srinumedisetti35@gmail.com>
2021-11-13 11:58:59 +0530

    delete operation

--- src/main/java/com/app/controller/SpecializationController.java
+++ src/main/java/com/app/controller/SpecializationController.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.app.entity.Specialization;
 import com.app.service.ISpecializationService;
@@ -16,16 +20,46 @@ import com.app.service.ISpecializationService;
 public class SpecializationController {
 	@Autowired
 	private ISpecializationService service;
-	
+
 	/**
-	 * 1.display all specialization
+	 * 1.show register page
+	 */
+	@GetMapping("/register")
+	public String displayRegister() {
+		return "SpecializationRegister";
+	}
+
+	/**
+	 * 2.on submit form save data
+	 */
+	@PostMapping("/create")
+	public String saveForm(@ModelAttribute Specialization specialization, Model model) {
+		Long id = service.saveSpecialization(specialization);
+		String message = "Record " + id + " is created successfully!";
+		model.addAttribute("message", message);
+		return "SpecializationRegister";
+	}
+
+	/**
+	 * 3.display all specialization
 	 */
 	@GetMapping("/all")
-	public String viewAll(Model model) {
-		List<Specialization> list=service.getAllSpecialization();
-		model.addAttribute("list",list);
+	public String viewAll(Model model, @RequestParam(value = "message", required = false) String message) {
+		List<Specialization> list = service.getAllSpecialization();
+		model.addAttribute("list", list);
+		model.addAttribute("message", message);
 		return "SpecializationData";
 	}
-	
+
+	/**
+	 * 4.delete data based on id
+	 */
+	@GetMapping("/delete")
+	public String removeData(@RequestParam Long id, RedirectAttributes attributes) {
+		service.removeSpecialization(id);
+		String message = "Record " + id + " is deleted successfully!";
+		attributes.addAttribute("message", message);
+		return "redirect:all";
+	}
 
 }
--- src/main/java/com/app/entity/Specialization.java
+++ src/main/java/com/app/entity/Specialization.java
@@ -10,21 +10,22 @@ import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name="spec_tab")
+@Table(name = "spec_tab")
 public class Specialization {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name="spec_id_col")
+	@Column(name = "spec_id_col")
 	private Long id;
-	@Column(name="spec_code_col")
+	@Column(name = "spec_code_col", length = 10, nullable = false, unique = true)
 	private String specCode;
-	@Column(name="spec_name_col")
+	@Column(name = "spec_name_col", length = 50, nullable = false, unique = true)
 	private String specName;
-	@Column(name="spec_note_col")
+	@Column(name = "spec_note_col", length = 250, nullable = false)
 	private String specNote;
 
 }
